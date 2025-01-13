9c0b216b77c50acbe3ae37aa7601cf97248f2917
GURU-PRASAD-BC <guruprasadbc.21it@kongu.edu>
2025-01-13 12:03:34 +0530

    upt

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -4,7 +4,7 @@ const {getUserActivities, logActivity} = require("../controllers/activityControl
 
 // Get recent activities for a user
 router.get("/",getUserActivities);
-// Log a new activity (can be used internally for different actions like adding expenses or groups)
+// Log a new activity (can be used internally for different actions like adding expenses or in groups)
 router.post("/log",logActivity);
 
 module.exports = router;
