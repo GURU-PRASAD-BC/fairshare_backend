7184df118c666b921356a391bbbc6d47473a07ee
GURU-PRASAD-BC <guruprasadbc.21it@kongu.edu>
2025-01-20 12:18:54 +0530

    update

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {getUserActivities, logActivity} = require("../controllers/activityController");
+const authenticateUser = require('../middlewares/validateUser');
 
 // Get recent activities for a user
-router.get("/",getUserActivities);
+router.get("/",authenticateUser,getUserActivities);
 // Log a new activity (can be used internally for different actions like adding expenses or in groups)
-router.post("/log",logActivity);
+router.post("/log",authenticateUser,logActivity);
 
 module.exports = router;
