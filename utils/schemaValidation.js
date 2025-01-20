abae2df0a767415961f566b1a07ed1a29e2ec391
GURU-PRASAD-BC <guruprasadbc.21it@kongu.edu>
2025-01-20 22:47:06 +0530

    update

--- utils/schemaValidation.js
+++ utils/schemaValidation.js
@@ -5,6 +5,7 @@ const signUpSchema = Joi.object({
   name: Joi.string().min(3).max(50).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
+  phone: Joi.string().optional()
 });
 
 // Sign-In Validation 
